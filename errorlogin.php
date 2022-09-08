7da9a7522c1e48e8337669074da02fbad5edadbe
Josue Martinez <josue.mart@jmcsoluciones.com>
2022-09-08 18:22:17 -0500

    Se agregan los archivos del proyecto terminado

--- errorlogin.php
+++ errorlogin.php
@@ -3,7 +3,7 @@ include('header.php')
 ?>
    
     <section class="content">
-        <h1>Bienvenido al Centro Médico UNADM</h1>
+        <h1>Favor de verificar sus datos de ingreso.</h1>
     </section> 
 <?php 
     include('footer.php');
